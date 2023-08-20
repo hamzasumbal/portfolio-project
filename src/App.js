27fc0e68cf6f352b75675655aed33bae93791952
hamzasumbal <msumbal.bee15seecs@seecs.edu.pk>
2023-08-20 13:40:20 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-   <div>
-    <h1>My Portfolio Project</h1>
-    <section>
-      <h2>About me</h2>
-    <p>My name is Hamza</p>
-    <p>I am a frontend developer</p>
-    </section>
-   </div>
+    <div>
+      <h1>My Portfolio Project</h1>
+      <section>
+        <h2>About me</h2>
+        <p>My name is Hamza</p>
+        <p>I am a frontend developer</p>
+      </section>
+    </div>
   );
 }
 
